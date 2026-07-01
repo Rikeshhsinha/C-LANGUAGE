2710b66957f8e9e7020edd1d2cfab327a8599816
Rikesh Kumar <rikeshsinha245@gmail.com>
2026-07-01 18:52:21 +0530

    Write a Program to give grades to a student

--- failOrPass.c
+++ failOrPass.c
@@ -6,16 +6,20 @@ int main()
 
     int marks;
 
-    printf("Enter your marks: ");
+    printf("Enter your marks(1-100): ");
     scanf("%d", &marks);
 
-    if (marks > 30)
+    if (marks > 30 && marks <= 100)
     {
         printf("You have passed the exam.\n");
     }
-    else
+    else if (marks >= 1 && marks < 30)
     {
         printf("You have failed the exam.\n");
+    }
+    else
+    {
+        printf("Invalid marks entered.\n");
     }
 
     return 0;
