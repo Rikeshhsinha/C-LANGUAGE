25ad991b0b00e7711d63809bd5e5341d491c7858
Rikesh Kumar <rikeshsinha245@gmail.com>
2026-07-01 18:58:37 +0530

    Write a Program to give grades to a student

---
+++ gradeOfSudent.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+// Write a Program to give grades to a student
+// marks < 30 is C
+// 30 <= marks < 70 is B
+// 70 <= marks < 90 is A
+// 90 <= marks <= 100 is A+
+
+
+int main (){
+
+    int marks;
+
+    printf("Enter your marks: ");
+    scanf("%d", &marks);
+
+    if (marks < 30)
+    {
+        printf("Your grade is C.\n");
+    }
+    else if (marks >= 30 && marks < 70)
+    {
+        printf("Your grade is B.\n");
+    }
+    else if (marks >= 70 && marks < 90)
+    {
+        printf("Your grade is A.\n");
+    }
+    else if (marks >= 90 && marks <= 100)
+    {
+        printf("Your grade is A+.\n");
+    }
+    else
+    {
+        printf("Invalid marks entered.\n");
+    }
+
+    return 0;
+}
