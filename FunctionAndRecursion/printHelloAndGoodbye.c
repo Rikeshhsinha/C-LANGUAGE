0122ea45b408f7b9bec6151da43bd4d0bf7a20fc
Rikesh Kumar <rikeshsinha245@gmail.com>
2026-07-03 00:13:19 +0530

    Add a program to print Hello, World! and Goodbye, World! using a function.

---
+++ FunctionAndRecursion/printHelloAndGoodbye.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+//Add a program to print "Hello, World!" and "Goodbye, World!" using a function.
+
+void printhelloAndgoodbye(){
+
+    printf("Hello, World!\n");
+    printf("Goodbye, World!\n");
+}
+
+int main()
+{
+printhelloAndgoodbye();
+   
+    return 0;
+}
