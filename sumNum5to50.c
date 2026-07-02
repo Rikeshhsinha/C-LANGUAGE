a9a2ee36daf7d7371ae043933d49004f1718c307
Rikesh Kumar <rikeshsinha245@gmail.com>
2026-07-02 22:12:16 +0530

    Add a program to calculate the sum of numbers from 5 to 50.

---
+++ sumNum5to50.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+//Add a program to calculate the sum of numbers from 5 to 50.
+
+int main(){
+
+int sum =0;
+
+for(int i =5; i <= 50; i++){
+    
+    sum = sum + i;
+
+
+}
+
+printf("The sum From 5 to 50 is : %d", sum);
+
+    return 0;
+}
