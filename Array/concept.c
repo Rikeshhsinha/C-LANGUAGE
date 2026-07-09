185f9183d4ede94d07c89ae22524217929da2084
Rikesh Kumar <rikeshsinha245@gmail.com>
2026-07-09 10:32:22 +0530

    Add the concepts of an array

---
+++ Array/concept.c
@@ -0,0 +1,5 @@
+//What is an array ??
+//An array is a collection of variables of the same type that are stored in contiguous memory locations
+
+//int marks[5] = {90, 80, 70, 60, 50}; // An array of integers with 5 elements
+//int student[n] = {90, 80, 70, 60, 50}; // An array of integers with n elements
