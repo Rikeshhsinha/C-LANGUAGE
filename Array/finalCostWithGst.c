3f9f84b0e95583942a3f7f682ce8ea8837684932
Rikesh Kumar <rikeshsinha245@gmail.com>
2026-07-09 20:24:27 +0530

    Add take input price of an item and print output of the total cost with GST in C language

---
+++ Array/finalCostWithGst.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+// Add three items in an array and print the total cost with GST in C language.
+int main()
+{
+
+    int n;
+    printf("Enter the number of items: ");
+    scanf("%d", &n);
+    int num[n];
+
+    for (int i = 0; i < n; i++)
+    {
+
+        printf("Enter the price of item %d: ", i);
+        scanf("%d", &num[i]);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+
+        printf("The price of item %d is: %d\n", i, num[i]);
+        printf("The total cost with GST is: %.2f\n", num[i] * 1.18);
+    }
+
+    return 0;
+}
