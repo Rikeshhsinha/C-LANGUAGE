4a2ed660d029c189c028f28d73d0d8f4d1c6cf21
Rikesh Kumar <rikeshsinha245@gmail.com>
2026-07-09 10:24:44 +0530

    Add the concept of pointer

--- Pointers/txt.c
+++ Pointers/txt.c
@@ -3,3 +3,17 @@
 
 //   if age = 22 : stored in 2010 
 //   then the ptr is : 2010 stored in 2013:
+
+#include <stdio.h>
+
+int main() {
+    int age = 22;
+    int *ptr = &age;
+
+    int value = *ptr;   // Dereferencing
+
+    printf("Age = %d\n", age);
+    printf("Value = %d\n", value);
+
+    return 0;
+}
